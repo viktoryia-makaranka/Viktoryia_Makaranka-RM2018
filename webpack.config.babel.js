2f35876c2a7dced644a033e02ebbec96ff9b19a9
Viktoryia_Makaranka <viktoryia.makaranka@burberry.com>
2018-04-24 14:59:42 +0300

    hometask2 done

---
+++ webpack.config.babel.js
@@ -0,0 +1,64 @@
+import  path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { HotModuleReplacementPlugin } from 'webpack'
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production'
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: ['./index.js'],
+
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+
+    output: {
+      path: path.resolve(__dirname, 'docs'),
+      publicPath: '',
+      filename: 'js/[name].bundle.js',
+      chunkFilename: 'js/[name].bundle.js',
+      sourceMapFilename: 'js/[name].map'
+    },
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minSize: 0,
+            minChunks: 2
+          }
+        }
+      }
+    },
+
+    devServer: {
+      contentBase: './docs',
+      hot: true,
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+      }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Vika App',
+        hash: true,
+        template: './index.html'
+      }),
+      new HotModuleReplacementPlugin()
+    ]
+  }
+}
